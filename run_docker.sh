2737eb6ed4515295195148e30ecdfe3b614191ef
Santiago Echeverri L. <santy.e@gmail.com>
2025-02-05 08:10:36 -0600

    Delete run_docker.sh

--- run_docker.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Cambia al directorio 'docker'
-cd docker || { echo "No se pudo cambiar al directorio 'docker'"; exit 1; }
-
-# Ejecuta el contenedor Docker con las variables de entorno definidas en .env
-docker run --env-file .env 970206120973.dkr.ecr.us-east-2.amazonaws.com/ept-vision-labeler 
